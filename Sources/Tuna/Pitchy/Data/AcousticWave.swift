666421e6f2457f96e31a2518ae148c251aabbfc5
Vasilis Akoinoglou <alladinian@gmail.com>
2020-07-19 15:49:43 +0300

    Added some documentation

--- Sources/Tuna/Pitchy/Data/AcousticWave.swift
+++ Sources/Tuna/Pitchy/Data/AcousticWave.swift
@@ -1,42 +1,61 @@
 public struct AcousticWave {
-  public static let speed: Double = 343
 
-  public let frequency: Double
-  public let wavelength: Double
-  public let period: Double
+    /// The speed of sound in air (m/s)
+    public static let speed: Double = 343
 
-  public var harmonics: [Pitch] {
-    var pitches = [Pitch]()
+    /// The frequency of the wave
+    public let frequency: Double
 
-    do {
-      for index in 1...16 {
-        try pitches.append(Pitch(frequency: Double(index) * frequency))
-      }
-    } catch {}
+    /// The wavelength
+    public let wavelength: Double
 
-    return pitches
-  }
+    /// The period of the wave
+    public let period: Double
 
-  // MARK: - Initialization
+    /// 16 harmonic pitches
+    public var harmonics: [Pitch] {
+        var pitches = [Pitch]()
 
-  public init(frequency: Double) throws {
-    try FrequencyValidator.validate(frequency: frequency)
-    self.frequency = frequency
-    wavelength = try WaveCalculator.wavelength(forFrequency: frequency)
-    period = try WaveCalculator.period(forWavelength: wavelength)
-  }
+        do {
+            for index in 1...16 {
+                try pitches.append(Pitch(frequency: Double(index) * frequency))
+            }
+        } catch {
+            debugPrint(error)
+        }
 
-  public init(wavelength: Double) throws {
-    try WaveCalculator.validate(wavelength: wavelength)
-    self.wavelength = wavelength
-    frequency = try WaveCalculator.frequency(forWavelength: wavelength)
-    period = try WaveCalculator.period(forWavelength: wavelength)
-  }
+        return pitches
+    }
 
-  public init(period: Double) throws {
-    try WaveCalculator.validate(period: period)
-    self.period = period
-    wavelength = try WaveCalculator.wavelength(forPeriod: period)
-    frequency = try WaveCalculator.frequency(forWavelength: wavelength)
-  }
+    // MARK: - Initialization
+
+    /// Initialize a wave with a frequency
+    /// - Parameter frequency: The frequency of the wave
+    /// - Throws: An error in case wavelength or period cannot be calculated
+    public init(frequency: Double) throws {
+        try FrequencyValidator.validate(frequency: frequency)
+        self.frequency = frequency
+        wavelength     = try WaveCalculator.wavelength(forFrequency: frequency)
+        period         = try WaveCalculator.period(forWavelength: wavelength)
+    }
+
+    /// Initialize a wave with a wavelength
+    /// - Parameter wavelength: The wavelength
+    /// - Throws: An error in case frequency or period cannot be calculated
+    public init(wavelength: Double) throws {
+        try WaveCalculator.validate(wavelength: wavelength)
+        self.wavelength = wavelength
+        frequency       = try WaveCalculator.frequency(forWavelength: wavelength)
+        period          = try WaveCalculator.period(forWavelength: wavelength)
+    }
+
+    /// Initialize a wave with a period
+    /// - Parameter period: The period of the wave
+    /// - Throws: An error in case wavelength or frequency cannot be calculated
+    public init(period: Double) throws {
+        try WaveCalculator.validate(period: period)
+        self.period = period
+        wavelength  = try WaveCalculator.wavelength(forPeriod: period)
+        frequency   = try WaveCalculator.frequency(forWavelength: wavelength)
+    }
 }
--- Sources/Tuna/Pitchy/FrequencyValidator.swift
+++ Sources/Tuna/Pitchy/FrequencyValidator.swift
@@ -1,16 +1,16 @@
 public struct FrequencyValidator {
-  public static var minimumFrequency = 20.0
-  public static var maximumFrequency = 4190.0
 
-  public static func isValid(frequency: Double) -> Bool {
-    return frequency > 0.0
-      && frequency >= minimumFrequency
-      && frequency <= maximumFrequency
-  }
+    public static var minimumFrequency = 20.0
+    public static var maximumFrequency = 4190.0
 
-  public static func validate(frequency: Double) throws {
-    if !isValid(frequency: frequency) {
-      throw PitchError.invalidFrequency
+    public static func isValid(frequency: Double) -> Bool {
+        frequency > 0.0 && (minimumFrequency ... maximumFrequency) ~= frequency
     }
-  }
+
+    public static func validate(frequency: Double) throws {
+        if !isValid(frequency: frequency) {
+            throw PitchError.invalidFrequency
+        }
+    }
+
 }
